312ba2ade9fbb4b5da6e8ee7b45e83bb943f491e
Shtykin <shtykin@mail.ru>
2024-03-09 01:41:34 +0800

    add layouts

--- app/src/main/java/ru/shtykin/antispam/MyCallScreeningService.java
+++ app/src/main/java/ru/shtykin/antispam/MyCallScreeningService.java
@@ -1,20 +1,15 @@
 package ru.shtykin.antispam;
 
-import android.annotation.SuppressLint;
-import android.app.ActivityManager;
-import android.graphics.Color;
 import android.graphics.PixelFormat;
 import android.telecom.Call;
 import android.telecom.CallScreeningService;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -22,21 +17,15 @@ import androidx.annotation.NonNull;
 import java.util.List;
 import java.util.Objects;
 
-import kotlin.text.Regex;
 import ru.shtykin.antispam.data.RepositoryImpl;
 import ru.shtykin.antispam.domain.Repository;
 
 public class MyCallScreeningService extends CallScreeningService {
-    private ViewGroup floatView;
-    private WindowManager.LayoutParams floatWindowLayoutParams;
-    private int LAYOUT_TYPE;
-    private WindowManager windowManager;
 
     @Override
     public void onScreenCall(@NonNull Call.Details details) {
         CallResponse.Builder response = new CallResponse.Builder();
         String phone = details.getHandle().getSchemeSpecificPart();
-        Log.e("DEBUG1", phone);
         respondToCall(details, response.build() );
 
         String output;
@@ -46,34 +35,18 @@ public class MyCallScreeningService extends CallScreeningService {
         } catch (Exception e) {
             output = phone;
         }
-//        WindowManager manager = (WindowManager) getSystemService(WINDOW_SERVICE);
-//        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
-//                180, // Ширина экрана
-//                180, // Высота экрана
-//                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY, // Говорим, что приложение будет поверх других. В поздних API > 26, данный флаг перенесен на TYPE_APPLICATION_OVERLAY
-//                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, // Необходимо для того чтобы TouchEvent'ы в пустой области передавались на другие приложения
-//                PixelFormat.TRANSLUCENT); // Само окно прозрачное
-//        RelativeLayout rootView = (RelativeLayout) LayoutInflater.from(this).inflate(R.layout.floating_window, null);
-//        manager.addView(rootView, params);
-
 
         DisplayMetrics metrics = getApplicationContext().getResources().getDisplayMetrics();
 
         int width = metrics.widthPixels;
-        int height = metrics.heightPixels;
 
-        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
-
+        WindowManager windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
         LayoutInflater inflater = (LayoutInflater) getBaseContext().getSystemService(LAYOUT_INFLATER_SERVICE);
-
-        floatView = (ViewGroup) inflater.inflate(R.layout.floating_window, null);
-
-        LAYOUT_TYPE = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
-
-        floatWindowLayoutParams = new WindowManager.LayoutParams(
+        ViewGroup floatView = (ViewGroup) inflater.inflate(R.layout.floating_window, null);
+        WindowManager.LayoutParams floatWindowLayoutParams = new WindowManager.LayoutParams(
                 ((int)(width * 0.9f)),
                 500,
-                LAYOUT_TYPE,
+                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
                 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
                 PixelFormat.TRANSLUCENT
         );
--- app/src/main/java/ru/shtykin/antispam/presentation/MainActivity.java
+++ app/src/main/java/ru/shtykin/antispam/presentation/MainActivity.java
@@ -1,40 +1,22 @@
 package ru.shtykin.antispam.presentation;
 
-import android.app.ActivityManager;
 import android.app.AlertDialog;
 import android.app.role.RoleManager;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.provider.Settings;
-import android.util.Log;
-
-import androidx.navigation.ui.AppBarConfiguration;
-
-import ru.shtykin.antispam.FloatingWindowApp;
-import ru.shtykin.antispam.R;
 import ru.shtykin.antispam.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.View;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
-
-    private AlertDialog alertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,70 +25,58 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-
-        Context context = this;
-        if (isServiceRunning()) {
-            stopService(new Intent(context, FloatingWindowApp.class));
-        }
-
-        binding.buttonPermissions.setOnClickListener(new View.OnClickListener() {
+        binding.buttonRequestWindow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                    checkPermissions();
-                }
-                if (checkOverlayPermission()) {
-
-                    Toast.makeText(MainActivity.this, "Все разрешения предоставлены", Toast.LENGTH_LONG).show();
-                } else {
+                if (!checkOverlayPermission()) {
                     requestFloatingWindowPermission();
                 }
             }
         });
 
-//        setSupportActionBar(binding.toolbar);
-
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-//        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-//        binding.fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-//                    checkPermissions();
-//                }
-//            }
-//        });
-    }
-
-    private boolean isServiceRunning() {
-        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-
-        for (ActivityManager.RunningServiceInfo service: manager.getRunningServices(Integer.MAX_VALUE) ) {
-            if (FloatingWindowApp.class.getName().equals(service.service.getClassName())) {
-                return true;
+        binding.buttonRequestDefault.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkPermissions();
             }
-        }
-        return false;
+        });
+
     }
 
     private void requestFloatingWindowPermission() {
-         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-         builder.setCancelable(true);
-         builder.setTitle("Screen Overlay Permission Needed");
-         builder.setMessage("Enable from settings");
-         builder.setPositiveButton("Open Settings", new DialogInterface.OnClickListener() {
-             @Override
-             public void onClick(DialogInterface dialogInterface, int i) {
-                 Intent intent = new Intent(
-                         Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                         Uri.parse("package:" + getPackageName())
-                 );
-                 startActivityForResult(intent, 1234);
-             }
-         });
-        alertDialog = builder.create();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle("Получайте информацию о звонках");
+        builder.setMessage("Разрешите E-Defender отображать всплывающие окна, чтобы видеть, кто вам звонит в момент вызова");
+        builder.setPositiveButton("Открыть настройки", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Intent intent = new Intent(
+                        Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + getPackageName())
+                );
+                startActivity(intent);
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    private void requestDefaultAppPermission() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle("Приложение по умолчания для АОН и защиты от спама");
+        builder.setMessage("Установите E-Defender приложением по умолчанию для АОН и защиты от спама");
+        builder.setPositiveButton("Открыть настройки", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Intent settingsIntent = new Intent();
+                settingsIntent.setAction(Settings.ACTION_MANAGE_DEFAULT_APPS_SETTINGS);
+                settingsIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(settingsIntent);
+            }
+        });
+        AlertDialog alertDialog = builder.create();
         alertDialog.show();
     }
 
@@ -114,65 +84,22 @@ public class MainActivity extends AppCompatActivity {
         return Settings.canDrawOverlays(this);
     }
 
-
-
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
-//
-//    @Override
-//    public boolean onSupportNavigateUp() {
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-//        return NavigationUI.navigateUp(navController, appBarConfiguration)
-//                || super.onSupportNavigateUp();
-//    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == 1001) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this, "Permission granted: " + 1001, Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(this, "Permission NOT granted: " + 1001, Toast.LENGTH_SHORT).show();
-            }
-
-            return;
-        }
-    }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == REQUEST_ID) {
-            if (resultCode == android.app.Activity.RESULT_OK) {
-                Log.e("DEBUG1", "OK");
-            } else {
-                Log.e("DEBUG1", "Failed");
+        if (requestCode == RC_DEFAULT_APP) {
+            if (resultCode != android.app.Activity.RESULT_OK) {
+                requestDefaultAppPermission();
             }
         }
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.Q)
     void checkPermissions() {
-
         RoleManager roleManager = (RoleManager) getSystemService(ROLE_SERVICE);
         Intent intent = roleManager.createRequestRoleIntent(RoleManager.ROLE_CALL_SCREENING);
-        startActivityForResult(intent, REQUEST_ID);
-
-
+        startActivityForResult(intent, RC_DEFAULT_APP);
     }
-    private static final int REQUEST_ID = 1;
+
+    private static final int RC_DEFAULT_APP = 1001;
 }
